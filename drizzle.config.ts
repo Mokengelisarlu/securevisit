fca395a236288c8686928f3a7f21d0dba9a68c4f
Anaclet Mwela <mokengelisarlu@gmail.com>
2026-08-21 16:01:41 +0200

    Master DB fix

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  out: './drizzle',
+  out: './db/migrations/master',
   schema: './db/master/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
